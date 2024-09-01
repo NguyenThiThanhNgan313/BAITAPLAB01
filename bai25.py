cb0ed7c731c984799dd1cb8d39651de5b4fdc040
NguyenThiThanhNgan313 <thanhngan.nguyen3103@gmail.com>
2024-09-01 18:51:15 +0700

    Add files via upload

---
+++ bai25.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 14:07:11 2024
+
+@author: ASUS
+"""
+
+a=input("Nhập một chữ cái:")
+if a.islower():
+    print(a.upper())
+else:
+    print(a.lower())
