b484f664ab448acfeecced4f4646b150bf432753
NguyenThiThanhNgan313 <thanhngan.nguyen3103@gmail.com>
2024-09-30 16:46:56 +0700

    Add files via upload

---
+++ sapxep.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 16:40:03 2024
+
+@author: ASUS
+"""
+
+ds = [('Tiền Giang', 63), ('Long An', 62), ('Vĩnh Long', 64), ('Bình Dương', 60)]
+print(sorted(ds))
